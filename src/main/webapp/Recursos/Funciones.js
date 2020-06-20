fa4ea2e199b266a392b042f69773a1e4207ad6f9
VikThor <VikThor@VikThor.home>
2020-06-20 16:53:53 -0500

    version 1

---
+++ src/main/webapp/Recursos/Funciones.js
@@ -0,0 +1,46 @@
+function validarForma(forma){
+    var usuario=forma.usuario;
+    if (usuario.valueOf===""||usuario.value==="Escribir Usuario"){
+    alert("Debe Proporcionar un nombre de usuario");
+    usuario.focus();
+    usuario.select();
+    return false;
+    }
+    var password=forma.password;
+    if(password.value===""|| password.value.length<3){
+        alert("Debe Proporcionar un Password al menos de 3 caracteres");
+        password.focus();
+        password.select();
+        return false;
+    }
+    var tecnologia=forma.tecnologia;
+    var checkSeleccionado=false;
+    for (var i = 0; i<tecnologia.length; i++){
+        if (tecnologia[i].checked){
+            checkSeleccionado=true;
+        }
+    }
+    if (!checkSeleccionado){
+        alert("Debe seleccionar una Tecnologia");
+        return false;
+    }
+    var genero=forma.genero;
+    var radioSeleccionado=false;
+    for (var i=0; i<genero.length;i++){
+        if (genero[i].checked){
+            radioSeleccionado=true;
+        }
+    }
+    if (!radioSeleccionado){
+        alert("Debe seleccionar un genero");
+        return false;
+    }
+    var ocupacion=forma.ocupacion;
+    if(ocupacion.value===""){
+        alert("Debe seleccionar una ocupacion");
+        return false;
+    }
+    //en este punto el formualario es valido
+    alert("Formulario valido, enviando datos al servidor");
+    return true;
+}
